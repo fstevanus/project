4084cd596c15a0ec1e148f2dad2605b1bf86c433
Fajar Stevanus <dracbros@gmail.com>
2018-08-22 16:19:57 +0700

    tambah request
    request

--- project/views.py
+++ project/views.py
@@ -1,4 +1,5 @@
-from flask import render_template
+import requests
+from flask import render_template, redirect, url_for, request
 from . import app
 
 @app.route('/')
@@ -8,6 +9,16 @@ def index():
     return render_template('index.html', title='Home', user=user)
 
 
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
 @app.route('/asem')
 def asem():
-    return "asem"
+    return render_template('tets.html')
+
+@app.route('/asem', methods = ['POST', 'GET'])
+def aseasm():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return requests.get('https://heygaron.serveo.net/getLocationxrequestx3nux1x0').content
